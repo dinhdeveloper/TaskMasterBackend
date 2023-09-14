12acb1e2209d1e4e7ad4800b07438162cfbb258d
DuyLP <tandv@unit.com.vn>
2023-09-14 10:13:56 +0700

    [DuyLP]

--- src/main/java/com/dinh/logistics/ultils/BaseResponse.java
+++ src/main/java/com/dinh/logistics/ultils/BaseResponse.java
@@ -1,17 +1,46 @@
 package com.dinh.logistics.ultils;
 
-import org.springframework.core.io.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.dinh.logistics.ultils.ApiError;
 
 @Data
-public class BaseResponse {
-	
+@NoArgsConstructor
+public class BaseResponse{
 	private int result_code;
 	private String result_description;
+	private ApiError error;
+	private List<ApiSubError> subErrors;
 	private Object data;
-	private int code_status;
-	private Resource resource;
 	
+	public BaseResponse(int result_code, String result_description, ApiError error) {
+		this.result_code = result_code;
+		this.result_description = result_description;
+		this.error = error;
+	}
+	public BaseResponse(int result_code, String result_description) {
+		this.result_code = result_code;
+		this.result_description = result_description;
+	}
 
+	public BaseResponse(int result_code, String result_description,Object data) {
+		this.result_code = result_code;
+		this.result_description = result_description;
+		this.data = data;
+	}
+	
+	private void addSubError(ApiSubError subError) {
+        if (subErrors == null) {
+            subErrors = new ArrayList<>();
+        }
+        subErrors.add(subError);
+    }
+	
+	abstract class ApiSubError {
+
+    }
 }
+
